de77997e70ae44cecc6a4cf1febce5869f153610
Miami <alex.miami.petrov@gmail.com>
2024-08-05 14:34:13 +0300

    m

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ImageCard from "./ImageCard";
-import "";
+import "../css/ImageGallery.css";
 
 const ImageGallery = ({ images }) => {
   if (images.length === 0) return null;
